8032ed9ad19fe8feb078624b6f37498e80e8a7fd
Shyamala Laxmi <shyamalakhammam816@gmail.com>
2026-09-22 15:29:33 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,72 @@
+# single line comment
+'''
+1.studentname
+2.studentbranch
+3.studentage
+4.studentgender
+
+'''
+
+
+student_name="vaishnavi"
+student_branch="cse"
+student_age="20"
+student_gender="female"
+
+
+print(student_name)
+print("student name is:",student_name)
+
+print(student_age)
+print("student age is:",student_age)
+
+print(student_branch)
+print("student branch is:",student_branch)
+
+print(student_gender)
+print("student gender is:",student_gender)
+
+# student name is vaishnavi
+# student branch is cse
+
+
+'''
+studentname
+fathername
+address
+mobilenumber
+intermarks
+islogin
+
+'''
+studentname="Shyamala"
+fathername="Parshuram"
+address="LB Nagar"
+mobilenumber="9324485826"
+intermarks="924"
+islogin="true"
+
+print(studentname)
+print("student name is:",studentname)
+print(fathername)   
+print("father name is:",fathername)
+print(address)
+print("address is:",address)
+print(mobilenumber)
+print("mobile number is:",mobilenumber)
+print(intermarks)
+print("inter marks is:",intermarks)
+print(islogin)
+print("is login:",islogin)
+
+a=10
+b=20
+print(a+b)
+#addition of a and b is 30
+print("addition of",a,"and",b,"is",a+b)
+print(f"addition of {a} and {b} is {a+b}")
+
+
+
+
+
